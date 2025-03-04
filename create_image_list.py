cd43fd827f6e313310c5f6cb27b73fb347a350a0
minhquanBr2 <minhquanhole03@gmail.com>
2025-03-04 10:59:53 +0700

    delete old files

--- create_image_list.py
+++
@@ -1,8 +0,0 @@
-import os
-
-prefix = "/kaggle/input/20191027/27"
-
-with open("eval_list.txt", "w") as f:
-    files = sorted(os.listdir("F:/LSCDATA/keyframes/201910/27"))
-    for file in files:
-        f.write(f"{prefix}/{file}\n")
